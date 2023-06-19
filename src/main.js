e7dafddb8c581c0485667b5fb4b9ffc3bf89c65e
Jyun <taiwanman1212@gmail.com>
2023-06-19 12:22:43 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import "./style/index.css"; //在此引入
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
